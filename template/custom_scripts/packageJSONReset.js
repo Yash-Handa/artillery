2d5ddcdedecbe304c2220cf70c5b1735242171ce
Yash Handa <yashhanda7@yahoo.com>
2020-01-14 01:42:12 +0530

    added eslintConfig and browserslist work arrounds

--- template/custom_scripts/packageJSONReset.js
+++ template/custom_scripts/packageJSONReset.js
@@ -5,5 +5,5 @@ if (p_json.eslintConfig || p_json.browserslist) {
   delete p_json.eslintConfig
   delete p_json.browserslist
 
-  fs.writeFile('../package.json', JSON.stringify(p_json, undefined, 2))
+  fs.writeFile('package.json', JSON.stringify(p_json, undefined, 2), () => {})
 }
